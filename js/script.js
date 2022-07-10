ccd2e5615c1f7fcfd5e603f3a8ef57c1e14891cd
Sergey <wer113wer@yandex.ru>
2022-07-10 15:05:03 +0300

    fifth block creation, realisation of footer
    updated svg icon size

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-// анимация переходов по блокам
-$("#menu").on("click", "a", function (event) {
-    event.preventDefault();
-    var id = $(this).attr('href'),
-        top = $(id).offset().top;
-    $('body,html').animate({ scrollTop: top }, 500);
+$(document).ready(function () {  
+    // анимация перехода по внутренним ссылкам
+    $("#menu").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 500);
+    });
 });
+
